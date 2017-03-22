c9008462acec0263415166ce1ea843ec4e00777b
By-Scar <adrien.chopard559@gmail.com>
2017-03-22 19:49:15 +0100

    Create my_strncpy.c

---
+++ my_strncpy.c
@@ -0,0 +1,20 @@
+#include "../include/my_strncpy.h"
+
+char    *my_strncpy(char *dest, char *src, int n)
+{
+  int   i;
+  int   x;
+
+  i = 0;
+  x = 0;
+  while (i < n)
+    {
+      dest[i] = src[i];
+      i = i + 1;
+    }
+  while (src[x] != 0)
+    x = x + 1;
+  if (n > x)
+    dest[i] = '\0';
+  return (dest);
+}
