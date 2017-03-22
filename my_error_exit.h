5c63111c12a14fac8aeb7b177bb7c5a5f3fd3db2
By-Scar <adrien.chopard559@gmail.com>
2017-03-22 18:33:10 +0100

    Create my_error_exit.h

---
+++ my_error_exit.h
@@ -0,0 +1,11 @@
+#ifndef MY_ERROR_EXIT_H_
+# define MY_ERROR_EXIT_H_
+
+#include <stdlib.h>
+#include "my_display.h"
+
+#  define ERR_WRITE     "Write error.\n"
+
+void    my_error_exit(char *err);
+
+#endif /* !MY_ERROR_EXIT_H_ */
